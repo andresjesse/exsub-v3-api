08c231063d7afd8a78a16d6666bbb6b399b35ef2
Andres <andres.porfirio@gmail.com>
2021-12-20 14:22:20 -0300

    added ace command create:user

---
+++ commands/CreateUser.ts
@@ -0,0 +1,46 @@
+import { BaseCommand, args } from "@adonisjs/core/build/standalone";
+
+export default class CreateUser extends BaseCommand {
+  /**
+   * Command name is used to run the command
+   */
+  public static commandName = "create:user";
+
+  @args.string({ description: "User email (unique)" })
+  public email: string;
+
+  @args.string({ description: "User password" })
+  public password: string;
+
+  /**
+   * Command description is displayed in the "help" output
+   */
+  public static description = "Create an User from CLI";
+
+  public static settings = {
+    /**
+     * Set the following value to true, if you want to load the application
+     * before running the command. Don't forget to call `node ace generate:manifest`
+     * afterwards.
+     */
+    loadApp: true,
+
+    /**
+     * Set the following value to true, if you want this command to keep running until
+     * you manually decide to exit the process. Don't forget to call
+     * `node ace generate:manifest` afterwards.
+     */
+    stayAlive: false,
+  };
+
+  public async run() {
+    const { default: User } = await import("App/Models/User");
+
+    await User.create({
+      email: this.email,
+      password: this.password,
+    });
+
+    this.logger.info("User created!");
+  }
+}
