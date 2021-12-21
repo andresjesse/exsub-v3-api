d7ba6396cc5ee87b1d7d28934e2a762d1725f1b8
Andres <andres.porfirio@gmail.com>
2021-12-21 10:45:05 -0300

    completed CRUD for team

--- app/Controllers/Http/TeamsController.ts
+++ app/Controllers/Http/TeamsController.ts
@@ -1,36 +1,39 @@
 import { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+import { schema } from "@ioc:Adonis/Core/Validator";
+
 import Team from "App/Models/Team";
+
+const teamSchema = schema.create({
+  description: schema.string({ trim: true }),
+});
 
 export default class TeamsController {
   public async index({}: HttpContextContract) {
     return await Team.all();
   }
 
-  public async create({ request }: HttpContextContract) {
-    // const description = request.input("description"); // QUAL A DIFERENÇA PRO STORE?
-    // const team = await Team.create({
-    //   description,
-    // });
-    // console.log("create");
-    // return team;
-  }
-
-  public async store({ request }: HttpContextContract) {
-    const description = request.input("description");
-
-    const team = await Team.create({
-      description,
-    });
-    console.log("create");
-
+  public async store({ request, response }: HttpContextContract) {
+    const payload = await request.validate({ schema: teamSchema });
+    const team = await Team.create(payload);
+    response.status(201);
     return team;
   }
 
-  public async show({}: HttpContextContract) {}
+  public async show({ params }: HttpContextContract) {
+    return await Team.findOrFail(params.id);
+  }
 
-  public async edit({}: HttpContextContract) {}
+  public async update({ request, params }: HttpContextContract) {
+    const team = await Team.findOrFail(params.id);
+    const payload = await request.validate({ schema: teamSchema });
+    team.description = payload.description;
+    await team.save();
+    return team;
+  }
 
-  public async update({}: HttpContextContract) {}
-
-  public async destroy({}: HttpContextContract) {}
+  public async destroy({ params }: HttpContextContract) {
+    const team = await Team.findOrFail(params.id);
+    await team.delete();
+    return team;
+  }
 }
