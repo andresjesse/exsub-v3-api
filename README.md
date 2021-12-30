3dd843e3771994404cb2b3ba10cfcf7a2e993865
Andres <andres.porfirio@gmail.com>
2021-12-30 14:09:31 -0300

    updated readme and deploy script

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # ExSub V3 API
 
 Adonis 5 study project/sandbox.
+
+(gitignore files backups are in drive)
--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,32 @@
 # RUN ON SERVER
 
+# 1-A First Run
+cd ~
+mkdir apps
+mkdir apps/exsub-v3-api
 cd ~/apps/exsub-v3-api
 git clone https://github.com/andresjesse/exsub-v3-api.git
-cd exsub-v3-api/
+
+# 1-B To Update production
+cd ~/apps/exsub-v3-api/exsub-v3-api
+git pull
 yarn
 node ace build --production --ignore-ts-errors
 
+# 2 Copy built code
 cp -rT ../production_base ../production
 cp -rT build ../production
 cd ../production
 yarn install --production
 node ace migration:run --force
 
-# First Run Init (in production directory):
+# 3 (First Run Only) Init (in production directory):
 mkdir tmp
+mkdir tmp/compilation
 node ace migration:run
 node ace create:admin <email> <password> admin
+
+# 4 (First Run Only) PM2 config (in production directory)
+pm2 start ecosystem.config.js
+pm2 startup # if not already configured to start at boot)
+pm2 save
